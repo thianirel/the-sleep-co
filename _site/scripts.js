72a9491aa27d6648c3c000ae78d60059901b5611
kenirel <thianirel@gmail.com>
2021-07-22 22:53:52 +0800

    Adding conditional statements on Gallery

--- _site/scripts.js
+++ _site/scripts.js
@@ -8,3 +8,13 @@ $(document).ready(function(){
     AOS.init();
 
 });
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+    speed: 500,
+    download: false
+});
